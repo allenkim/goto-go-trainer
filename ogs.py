a8612f8c125e046f3d0526d8e342bb04719e8590
Allen Kim <allenkim95@gmail.com>
2016-11-07 22:11:34 -0500

    Clean up oauth code

--- ogs.py
+++ ogs.py
@@ -1,13 +1,26 @@
 import json
 import requests
 
+'''
+private_client.py contains a dictionary called data that contains important client information:
+client_id
+client_secret
+grant_type
+username
+password
+'''
 import private_client
 
-r = requests.post('https://online-go.com/oauth2/access_token', data=private_client.data)
+def bytes_to_dict(req):
+    return json.loads(str(req.content,'utf-8'))
+
+oauth_req = requests.post('https://online-go.com/oauth2/access_token', data=private_client.data)
+oauth_token = bytes_to_dict(oauth_req)
 
 headers = {
-    "Authorization": "Bearer " + json.loads(str(r.content,'utf-8'))["access_token"]
+    "Authorization": "Bearer " + oauth_token["access_token"]
 }
 
-friends = requests.get('https://online-go.com/api/v1/me/friends', headers=headers)
-print(friends.content)
+stats_req = requests.get('https://online-go.com/api/v1/me/', headers=headers)
+stats = bytes_to_dict(stats_req);
+print(stats)
